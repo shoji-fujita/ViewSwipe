8f08df7ea0c07ff0dd0ff438d932b595f3c7dc8b
shoji-fujita <shoji.fujita.0819@gmail.com>
2016-12-07 00:20:03 +0900

    init

--- ViewSwipe/ViewController.swift
+++ ViewSwipe/ViewController.swift
@@ -10,16 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var targetView: UIView!
+    var startPointY: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        targetView.frame = view.frame
+        targetView.frame.origin.y = self.view.frame.size.height - 70
+        targetView.tag = 1
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
     }
-
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesMoved(touches, with: event)
+        
+        let touch = touches.first!
+        if touch.view?.tag != 1 {
+            return
+        }
+        let nowPointY = touch.location(in: targetView).y
+        let prePointY = touch.previousLocation(in: targetView).y
+        
+        targetView.frame.origin.y += nowPointY - prePointY
+        
+        view.alpha = CGFloat(targetView.frame.origin.y) / CGFloat(view.frame.size.height)
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        
+        let baseY = self.view.frame.size.height * 3/4
+        if targetView.frame.origin.y < baseY {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.targetView.frame.origin.y = 70
+                self.view.alpha = CGFloat(self.targetView.frame.origin.y) / CGFloat(self.view.frame.size.height)
+            })
+        } else {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.targetView.frame.origin.y = self.view.frame.size.height - 70
+                self.view.alpha = CGFloat(self.targetView.frame.origin.y) / CGFloat(self.view.frame.size.height)
+            })
+        }
+        
+    }
 
 }
-
